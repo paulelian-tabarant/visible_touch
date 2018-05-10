e722ef0f5b4e9b37d8cc0d575f53438493a50279
Cacciaguerra Adrien <adrien.caccia@gmail.com>
2018-05-10 16:23:23 +0200

    semantic added

--- frame-editor/src/App.js
+++ frame-editor/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Icon } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
@@ -13,6 +14,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Button icon>
+          <Icon name = 'lightbulb' />
+          Frame Editor
+        </Button>
       </div>
     );
   }
