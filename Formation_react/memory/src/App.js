e05f32495576fa2a440cac738bff1e7fc28ce1f7
Paul-Elian Tabarant <paul-elian.tabarant@telecom-paristech.fr>
2018-05-13 11:56:09 +0200

    Imported and set up the Memory game interface

--- Formation_react/memory/src/App.js
+++ Formation_react/memory/src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './Card'
+import GuessCount from './GuessCount'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    <div className="memory">
+      <GuessCount guesses={0} />
+      <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
+      <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+      <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
+      <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+      <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+      <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+    </div>
+    )
+  }
+
+  handleCardClick(card) {
+    console.log(card, 'clicked')
   }
 }
 
