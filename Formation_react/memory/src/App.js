4d5d2d74fc42277f581a3d77dd95f58b29d514ee
Paul-Elian Tabarant <paul-elian.tabarant@telecom-paristech.fr>
2018-05-13 22:40:55 +0200

    Implemented the Memory functional behaviour.

--- Formation_react/memory/src/App.js
+++ Formation_react/memory/src/App.js
@@ -1,26 +1,102 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import shuffle from 'lodash.shuffle'
+
+import './App.css'
+
 import Card from './Card'
 import GuessCount from './GuessCount'
+import HallOfFame, { FAKE_HOF } from './HallOfFame'
+
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
+const VISUAL_PAUSE_MSECS = 750
 
 class App extends Component {
+  state = {
+    cards: this.generateCards(),
+    currentPair: [],
+    guesses: 0,
+    matchedCardIndices: [],
+  }
+
+  generateCards() {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = shuffle(SYMBOLS)
+    while (result.length < size) {
+      const card = candidates.pop()
+      result.push(card, card)
+    }
+    return shuffle(result)
+  }
+
+  // Arrow fx for binding
+  handleCardClick = index => {
+    const { currentPair } = this.state
+
+    if(currentPair.length === 2) {
+      return
+    }
+
+    if(currentPair.length === 0) {
+      this.setState({ currentPair : [index] })
+      return
+    }
+    // if we double click on the same card
+    if(currentPair.length === 1 && index === currentPair[0]) {
+      return
+    }
+    this.handleNewPairClosedBy(index)
+  }
+
+  handleNewPairClosedBy(index) {
+    const { cards, currentPair, guesses, matchedCardIndices } = this.state
+
+    const newPair = [currentPair[0], index]
+    const newGuesses = guesses + 1
+    const matched = cards[newPair[0]] === cards[newPair[1]]
+    this.setState({ currentPair: newPair, guesses: newGuesses })
+    if(matched) {
+      this.setState({ matchedCardIndices: [...matchedCardIndices, ...newPair]})
+    }
+    setTimeout(() => this.setState({ currentPair: [] }), VISUAL_PAUSE_MSECS)
+  }
+
+  getFeedbackFromCard(index) {
+    const { currentPair, matchedCardIndices } = this.state
+    const indexMatched = matchedCardIndices.includes(index)
+
+    if (currentPair.length < 2) {
+      return indexMatched || index === currentPair[0] ? 'visible' : 'hidden'
+    }
+
+    if(currentPair.includes(index)) {
+      return indexMatched ? 'justMatched' : 'justMismatched'
+    }
+
+    return indexMatched ? 'visible' : 'hidden'
+  }
+
   render() {
+    const { cards, guesses, matchedCardIndices } = this.state
+    const won = matchedCardIndices === cards.length
     return (
     <div className="memory">
-      <GuessCount guesses={0} />
-      <Card card="😀" feedback="hidden" onClick={this.handleCardClick} />
-      <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
-      <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick} />
-      <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
-      <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
-      <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+      <GuessCount guesses={guesses} />
+      {cards.map((card, index) => (
+        <Card
+          card={card}
+          feedback={this.getFeedbackFromCard(index)}
+          index={index}
+          key={index}
+          onClick={this.handleCardClick}
+        />
+      ))}
+      {won && <p>GAGNÉ!</p>}
+      <HallOfFame entries={FAKE_HOF} />
     </div>
     )
   }
-
-  handleCardClick(card) {
-    console.log(card, 'clicked')
-  }
 }
 
-export default App;
+export default App
--- Formation_react/memory/src/Card.js
+++ Formation_react/memory/src/Card.js
@@ -1,15 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import './Card.css'
 
 const HIDDEN_SYMBOL = '❓'
 
-const Card = ({ card, feedback, onClick }) => (
-    <div className={`card ${feedback}`} onClick={() => onClick(card)}>
+const Card = ({ card, feedback, index, onClick }) => (
+    <div className={`card ${feedback}`} onClick={() => onClick(index)}>
       <span className="symbol">
         {feedback === 'hidden' ? HIDDEN_SYMBOL : card}
       </span>
     </div>
   )
+
+// Defines which type of values should be affected to the props
+// of the Card Component.
+Card.propTypes = {
+    card: PropTypes.string.isRequired,
+    feedback: PropTypes.oneOf([
+        'hidden',
+        'justMatched',
+        'visible',
+        'justMismatched',
+    ]).isRequired,
+    index: PropTypes.number.isRequired,
+    onClick: PropTypes.func.isRequired,
+}
 
 export default Card
