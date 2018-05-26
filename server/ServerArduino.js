0c76c621c8f268c179643d094d8a2b19a6e24ca9
Cacciaguerra Adrien <adrien.caccia@gmail.com>
2018-05-26 15:30:31 +0200

    communication server started

---
+++ server/ServerArduino.js
@@ -0,0 +1,66 @@
+var SerialPort = require('serialport'); // include the serialport library
+var http = require('http');
+const qs = require('querystring');
+
+var myPort = new SerialPort("COM4", {
+  baudRate: 9600,
+  dataBits: 8,
+  parity: 'none',
+  stopBits: 1,
+  flowControl: false
+}); // open the port
+myPort.on('open', openPort); // called when the serial port opens
+var ready = false;
+console.log("blabla");
+
+var shouldWrite = false;
+var dataToWrite;
+
+function openPort() {
+  ready = true;
+  console.log('port open');
+  //console.log('baud rate: ' + myPort.options.baudRate);
+
+  myPort.on('data', function (data) {
+    console.log(data.toString());
+    shouldWrite = false;
+  });
+
+  writeInterval = setInterval(writeData, 1000);
+}
+
+function writeData() {
+  console.log("---------");
+  if (shouldWrite) {
+    myPort.write("-1 ");
+    for (var i = 0; i < 180; i++) {
+      myPort.write(dataToWrite[i].toString());
+      myPort.write(" ");
+    }
+    myPort.write("-2 ");
+    myPort.write(dataToWrite.length.toString() + " ");
+  }
+}
+
+function startWriting() {
+  shouldWrite = true;
+}
+
+function sendDataToArduino(data) {
+  dataToWrite = data;
+  startWriting();
+}
+
+var serv = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  if (req.method === 'POST') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString(); // convert Buffer to string
+    });
+    req.on('end', () => {
+      res.write(JSON.stringify(qs.parse(body)));
+      res.end('ok');
+    });
+  }
+}).listen(8000);
