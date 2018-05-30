0f6180f9d4342da1731493657bac98233c83fb26
Paul-Elian Tabarant <paul-elian.tabarant@telecom-paristech.fr>
2018-05-30 15:47:07 +0200

    Added a field to type delay for each frame Set to 250ms as default

--- frame-editor/src/components/FrameSliders.js
+++ frame-editor/src/components/FrameSliders.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
 import Slider from 'react-rangeslider';
+import { Input, Button } from 'semantic-ui-react'
 import 'react-rangeslider/lib/index.css'
 
 class FrameSliders extends Component {
   constructor(props, context) {
     super(props, context);
+    const { defaultDelay } = this.props;
+    let defaultDelays = new Array(props.frames).fill(defaultDelay);
     this.state = {
       frames: props.frames,
       current: props.current,
+      delays: defaultDelays,
+      inputValue: "",
     };
   }
 
   handleFrameChange = (value) => {
     this.setState({
-      current: value
+      current: value,
+      inputValue: "",
     });
     this.props.changeCurrentFrame(value);
   }
 
+  handleSetDelay = () => {
+    const { current, delays, inputValue } = this.state;
+    let newDelays = delays.slice();
+    newDelays[current] = parseInt(inputValue);
+    this.setState({
+      delays: newDelays,
+    });
+  }
+
+  handleInputChange = (event) => {
+    this.setState({
+      inputValue: event.target.value
+    })
+  }
+
   render() {
-    let { frames, current } = this.state;
+    let { frames, current, delays, inputValue } = this.state;
+    let curFrameDelay = delays[current];
     return (
       <div className="slider-group">
         <Slider
@@ -30,6 +52,16 @@ class FrameSliders extends Component {
           onChange={this.handleFrameChange}
         />
         <div className="value">Frame: {current}</div>
+        <div className="delay-input-group">
+          <Input 
+            className="delay-input-field"
+            value={inputValue}
+            placeholder={`Current delay : ${curFrameDelay}ms`} action
+            onChange={this.handleInputChange}>
+            <input />
+            <Button className="apply-delay" onClick={this.handleSetDelay}>Apply</Button>
+          </Input>
+        </div>
       </div>
     )
   }
